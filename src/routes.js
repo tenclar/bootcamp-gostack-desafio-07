8e965f77077889198a279fdc19f5c3270be9c8a0
Tenclar <tenclarvalus@gmail.com>
2019-11-10 15:58:28 -0500

    Estilização do Header

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Main from './pages/Main';
+import Cart from './pages/Cart';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={Main} />
+      <Route path="/cart" component={Cart} />
+    </Switch>
+  );
+}
