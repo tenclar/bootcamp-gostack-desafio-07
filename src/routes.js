5e042f1abaea43f39d3f740539185c159f9f98f7
Tenclar <tenclarvalus@gmail.com>
2019-11-12 14:45:35 -0500

    configuração do reactotron

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Main from './pages/Main';
+import Home from './pages/Home';
 import Cart from './pages/Cart';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/" exact component={Main} />
+      <Route path="/" exact component={Home} />
       <Route path="/cart" component={Cart} />
     </Switch>
   );
